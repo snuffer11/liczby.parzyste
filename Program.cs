3ff4f448b438e007184e4803b5ca3a9d3698d1a6
snuffer11 <snuffer1992@gmail.com>
2021-06-02 19:41:07 +0200

    add

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace liczby_parzyste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj podaj liczbe do sprawdzenia");
+            while (true)
+            {
+                int number = GetInput();
+                if (number%2==1)
+                {
+                    Console.WriteLine("liczba jest nieparzysta");
+
+                }
+                else if(number%2 == 0)
+                {
+                    Console.WriteLine("liczba jest parzysta");
+                }
+
+                Console.WriteLine("Podaj następna liczbe lub wpisz 'e' żeby wyjść");
+            }
+        }
+
+        private static int GetInput()
+        {
+            while (true)
+            {
+                var val = Console.ReadLine();
+                if (val.ToUpper() == "E")
+                {
+                    Environment.Exit(0);
+                }
+                if (!int.TryParse(val,out int number))
+                {
+                    Console.WriteLine("Podales złe dane");
+                    continue;
+                }
+                return number;
+            }
+            
+        }
+    }
+}
